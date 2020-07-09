fcad12dc64252881c8c2c8978bead4f9fa83c023
Vinod <vinodkumarrj565@gmail.com>
2020-07-09 17:14:21 +0530

    Add all Project files

---
+++ batch_s26.bat
@@ -0,0 +1,37 @@
+load --library ../expl/library.lib
+load --exhandler ../spl/spl_progs/exhandler.xsm
+load --int=console ../spl/spl_progs/sample_intconsole.xsm
+load --int=disk ../spl/spl_progs/sample_intdisk.xsm
+load --int=timer ../spl/spl_progs/sample_timer.xsm
+load --os ../spl/spl_progs/os_startup_s26.xsm
+load --idle ../expl/expl_progs/sample_idle.xsm
+load --init ../expl/expl_progs/login.xsm
+load --shell ../expl/expl_progs/shell_s26.xsm
+load --data ../sample.dat
+load --exec ../expl/expl_progs/odd.xsm
+load --exec ../expl/expl_progs/ls.xsm
+load --exec ../expl/expl_progs/as1_s24.xsm
+load --exec ../expl/expl_progs/as1_s25.xsm
+load --exec ../expl/expl_progs/as1_s23.xsm
+load --exec ../expl/expl_progs/as2_s23.xsm
+load --int=4 ../spl/spl_progs/int_4_s23.xsm
+load --int=5 ../spl/spl_progs/int5_s25.xsm
+load --int=6 ../spl/spl_progs/int6_s24.xsm
+load --int=7 ../spl/spl_progs/int7_s25.xsm
+load --int=8 ../spl/spl_progs/sample_int8.xsm
+load --int=9 ../spl/spl_progs/sample_int9.xsm
+load --int=10 ../spl/spl_progs/int_10_s20.xsm
+load --int=11 ../spl/spl_progs/sample_int11.xsm
+load --int=12 ../spl/spl_progs/int12_s26.xsm
+load --int=13 ../spl/spl_progs/int13_s22.xsm
+load --int=14 ../spl/spl_progs/int14_s22.xsm
+load --int=15 ../spl/spl_progs/int_15_s26.xsm
+load --int=16 ../spl/spl_progs/int16_s26.xsm
+load --int=17 ../spl/spl_progs/int17_s26.xsm
+load --module 0 ../spl/spl_progs/module_0_s24.xsm
+load --module 1 ../spl/spl_progs/module_1_s25.xsm
+load --module 2 ../spl/spl_progs/module_2_s25.xsm
+load --module 3 ../spl/spl_progs/module_3_s25.xsm
+load --module 4 ../spl/spl_progs/MOD_4.xsm
+load --module 5 ../spl/spl_progs/MOD_5.xsm
+load --module 7 ../spl/spl_progs/mod_7_s26.xsm
